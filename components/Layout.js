3e552fd79a97119aae42333b656dc4de4723cfba
hardik6869 <sorthiyahardik6869@gmail.com>
2022-11-11 12:45:15 +0530

    Header & Nav Component

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,9 @@
+import Header from "./Header";
+
 const Layout = ({ children }) => {
   return (
     <div>
+      <Header />
       <p> I am the Layout Component. </p>
       {children}
     </div>
