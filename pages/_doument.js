77ad1cac0b6bacd48fccdce3357e1e3874c9ecca
hardik6869 <sorthiyahardik6869@gmail.com>
2022-11-16 10:20:04 +0530

    Fixing Styled Components: Flicker on Server Render

--- pages/_doument.js
+++ pages/_doument.js
@@ -1,10 +1,21 @@
 /* eslint-disable @next/next/no-document-import-in-page */
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
+  static getInitialProps = ({ renderPage }) => {
+    const sheet = new ServerStyleSheet();
+    const Page = renderPage(
+      (App) => (props) => sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...Page, styleTags };
+  };
+
   render() {
     return (
       <Html lang="en-CA">
+        <Head />
         <body>
           <Main />
           <NextScript />
