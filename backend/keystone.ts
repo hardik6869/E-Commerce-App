c3a5d11a53b0cdf5485c2ccd15efc24361cd9307
hardik6869 <sorthiyahardik6869@gmail.com>
2022-11-16 16:36:14 +0530

    Backend: setting up keystone & typescript

---
+++ backend/keystone.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import { createSchema, config } from "@keystone-next/keystone/schema";
+
+const databaseURL = process.env.DATABASE_URL;
+const sessionConfig = {
+  maxAge: 60 * 60 * 25 * 360,
+  secret: process.env.COOCKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: { origin: [process.env.FRONTEND_URL], credentials: true },
+  },
+  db: { adapter: "mongoose", url: databaseURL },
+  lists: createSchema({
+    // Schema item go in here
+  }),
+  ui: {
+    // TODO: change,this for role
+    isAccessAllowed: () => true,
+  },
+});
