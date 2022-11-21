496bcc7c482296f093421a45062d2f1e2e31a3ee
hardik6869 <sorthiyahardik6869@gmail.com>
2022-11-21 14:32:53 +0530

    backend: Two Way Data Relationship in Keystone

--- backend/schemas/Product.ts
+++ backend/schemas/Product.ts
@@ -1,4 +1,4 @@
-import { text, select, integer } from "@keystone-next/fields";
+import { text, select, integer, relationship } from "@keystone-next/fields";
 import { list } from "@keystone-next/keystone/schema";
 export const Product = list({
   fields: {
@@ -6,6 +6,16 @@ export const Product = list({
     description: text({
       ui: {
         displayMode: "textarea",
+      },
+    }),
+
+    photo: relationship({
+      ref: "ProductImage.product",
+      ui: {
+        displayMode: "cards",
+        cardFields: ["image", "altText"],
+        inlineCreate: { fields: ["image", "altText"] },
+        inlineEdit: { fields: ["image", "altText"] },
       },
     }),
     status: select({
