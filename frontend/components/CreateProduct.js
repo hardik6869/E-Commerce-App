aa6d80345ec8387f5e241b4bf03e358afacaf047
hardik6869 <sorthiyahardik6869@gmail.com>
2022-11-24 18:05:37 +0530

    Create Products data using Mutation

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,5 +1,31 @@
 import useForm from "../lib/useForm";
 import Form from "./styles/Form";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/client";
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 const CreateProduct = () => {
   const { inputs, handleChange, resetForm, clearForm } = useForm({
@@ -8,13 +34,23 @@ const CreateProduct = () => {
     price: 31586,
     description: "These are the best Shoes",
   });
+
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
+
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
+        await createProduct();
+        clearForm();
       }}
     >
-      <fieldset>
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
           <input type="file" id="image" name="image" onChange={handleChange} />
