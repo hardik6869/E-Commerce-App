575ae07e5c5a457580c74ba619cd2ec429658dc6
hardik6869 <sorthiyahardik6869@gmail.com>
2022-12-13 11:43:21 +0530

    Frontend : Add Pagination Link

---
+++ frontend/components/Pagination.js
@@ -0,0 +1,43 @@
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+import PaginationStyles from "./styles/PaginationStyle";
+import DisplayError from "./ErrorMessage";
+import { perPage } from "../config";
+
+const PAGINATION_QUERY = gql`
+  query PAGINATION_QUERY {
+    _allProductsMeta {
+      count
+    }
+  }
+`;
+
+const Pagination = ({ page }) => {
+  const { data, loading, error } = useQuery(PAGINATION_QUERY);
+  if (loading) return "Loading...";
+  if (error) return <DisplayError error={error} />;
+  const { count } = data._allProductsMeta;
+  const pageCount = Math.ceil(count / perPage);
+  return (
+    <PaginationStyles>
+      <Head>
+        <title> Sick Fits {page} of ___ </title>
+      </Head>
+      <Link href={`/products/${page - 1}`}>
+        <a aria-disabled={page <= 1}> {"<<"} Prev </a>
+      </Link>
+      <p>
+        {page} Page of {pageCount}
+      </p>
+      <p> {count} Items Total </p>
+      <Link href={`/products/${page + 1}`}>
+        <a aria-disabled={page <= pageCount}> Next {">>"} </a>
+      </Link>
+    </PaginationStyles>
+  );
+};
+
+export default Pagination;
