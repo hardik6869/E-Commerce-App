3eb8b2bfa9b4b155028d03e50110d6aedb88aa54
hardik6869 <sorthiyahardik6869@gmail.com>
2022-12-28 16:25:43 +0530

    Permission & Create Access Function

--- backend/access.ts
+++ backend/access.ts
@@ -1,6 +1,26 @@
-import { ListAccessArgs } from "./types"
+import { ListAccessArgs } from "./types";
+import { permissionsList } from "./schemas/fields";
 
 // At it's simplest,the access control returns a yes or no value depending on the users session
-export const isSignedIn = ({session}: ListAccessArgs) => {
-    return !!session
-}
+export const isSignedIn = ({ session }: ListAccessArgs) => {
+  return !!session;
+};
+
+const generatePermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    ({ session }: ListAccessArgs) => {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+// Permissons check if someone meets a criteria - YES or NO
+export const Permission = {
+    ...generatePermissions,
+    isAwesome({session}: ListAccessArgs): boolean {
+        return session?.data.name?.includes('HARDIK')
+    }
+};
+
+// Rule based Function
