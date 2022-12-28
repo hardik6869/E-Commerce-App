14ddb69abbf65e7dcfa03107781de3e18932fc11
hardik6869 <sorthiyahardik6869@gmail.com>
2022-12-28 18:25:31 +0530

    backend: Get Roles- Roles based roles & Hide UI

--- backend/schemas/Role.ts
+++ backend/schemas/Role.ts
@@ -1,18 +1,29 @@
-import { relationship, text } from "@keystone-next/fields"
-import { list } from "@keystone-next/keystone/schema"
-import { permissionFields } from "./fields"
+import { relationship, text } from "@keystone-next/fields";
+import { list } from "@keystone-next/keystone/schema";
+import { permissions } from "../access";
+import { permissionFields } from "./fields";
 
 export const Role = list({
-    fields:{
-        name: text({isRequired:true}),
-        ...permissionFields,
-        assignedTo: relationship({
-            ref:'User.role', // TODO: Add this to the user
-            many: true,
-            ui:{
-                itemView: {fieldMode: 'read'}
-            }
-        })
-
-    }
-})
+  access: {
+    create: permissions.canManageRoles,
+    read: permissions.canManageRoles,
+    update: permissions.canManageRoles,
+    delete: permissions.canManageRoles,
+  },
+  ui:{
+    hideCreate: (args) => !permissions.canManageRoles(args),
+    hideDelete: (args) => !permissions.canManageRoles(args),
+    isHidden: (args) => !permissions.canManageRoles(args),
+  },
+  fields: {
+    name: text({ isRequired: true }),
+    ...permissionFields,
+    assignedTo: relationship({
+      ref: "User.role", // TODO: Add this to the user
+      many: true,
+      ui: {
+        itemView: { fieldMode: "read" },
+      },
+    }),
+  },
+});
